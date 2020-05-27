c230c349a1e4acb3f493dfe075aaf18d0b771d64
rishban <rishbandari97@gmail.com>
2020-05-27 16:18:48 -0300

    test

---
+++ test.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the home page bish");
+});
+const port = process.env.port || 3000;
+app.listen(3000, () => console.log('Listening on port 3000...'));
+
